ee68070ca3995e0757228e7be386610154a1e715
Engin Karataş <enginkaratas99@gmail.com>
2023-03-14 00:02:41 +0300

    authanticated user returning

--- server/functions/index.js
+++ server/functions/index.js
@@ -10,7 +10,7 @@ const twitterClient = new TwitterApi({
 });
 
 const callbackURL = "http://localhost:5000/tfa-backend/us-central1/callback";
- 
+
 exports.auth = functions.https.onRequest((request, response) => {
   const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
     callbackURL,
@@ -20,5 +20,70 @@ exports.auth = functions.https.onRequest((request, response) => {
 
   response.redirect(url);
 });
-exports.callback = functions.https.onRequest((request, response) => {});
-exports.tweet = functions.https.onRequest((request, response) => {});
+exports.auth = functions.https.onRequest((request, response) => {
+  const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
+    callbackURL,
+    { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] }
+  );
+
+  dbRef.set({ codeVerifier, state })
+    .then(() => {
+      response.redirect(url);
+    })
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Error setting code verifier and state');
+    });
+});
+
+exports.callback = functions.https.onRequest((request, response) => {
+  const { state, code } = request.query;
+
+  dbRef.get()
+    .then((dbSnapshot) => {
+      const { codeVerifier, state: storedState } = dbSnapshot.data();
+
+      if (state !== storedState) {
+        return response.status(400).send('Stored tokens do not match!');
+      }
+
+      return twitterClient.loginWithOAuth2({
+        code,
+        codeVerifier,
+        redirectUri: callbackURL,
+      });
+    })
+    .then(({ client: loggedClient, accessToken, refreshToken }) => {
+      return dbRef.set({ accessToken, refreshToken })
+        .then(() => loggedClient.v2.me());
+    })
+    .then(({ data }) => {
+      response.send(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Error refreshing OAuth2 token or fetching user data');
+    });
+});
+
+exports.tweet = functions.https.onRequest((request, response) => {
+  dbRef.get()
+    .then(({ data: { refreshToken } }) => {
+      return twitterClient.refreshOAuth2Token(refreshToken);
+    })
+    .then(({ client: refreshedClient, accessToken, refreshToken: newRefreshToken }) => {
+      return dbRef.set({ accessToken, refreshToken: newRefreshToken })
+        .then(() => openai.createCompletion('text-davinci-001', {
+          prompt: 'tweet something cool for #techtwitter',
+          max_tokens: 64,
+        }))
+        .then(({ data: { choices } }) => refreshedClient.v2.tweet(choices[0].text));
+    })
+    .then(({ data }) => {
+      response.send(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Error refreshing OAuth2 token or posting tweet');
+    });
+});
